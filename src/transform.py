1e21c06b05cce119c56b4402e41dbd63c6f81c98
Lebogang Lobakeng <lobakengl@gmail.com>
2026-09-21 14:48:33 +0200

    Adding a new transform function to categorize age

--- src/transform.py
+++ src/transform.py
@@ -1,3 +1,18 @@
+def categorize_age(customers):
+    for customer in customers:
+        if customer['age'] <= 30:
+            customer['age_group'] = 'Young Adult'
+            
+        elif customer['age'] <= 50:
+            customer['age_group'] = 'Adult'
+        
+        else:
+            customer['age_group'] = 'Mature'
+            
+    return customers
+
+
+
 def transform(customers):
 # we create a new list instead of modifying the extracted list directly
     transformed_customers = []
@@ -11,6 +26,12 @@ def transform(customers):
         
         # we then add the uptdated/transformed data to the empty list, transformed_customers
         transformed_customers.append(customer)
+    
+    transformed_customers = categorize_age(transformed_customers)
         
     return transformed_customers
+
+
+
+        
     
