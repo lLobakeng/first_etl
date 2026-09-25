e9a2c96d43741328ef278c190356ce76060f35e0
Lebogang Lobakeng <lobakengl@gmail.com>
2026-09-25 08:21:23 +0200

    Created a main gaurd for the main module

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from src.extract import extract
 from src.transform import transform
 from src.load import load
 
-customers = extract()
-
-transformed_customers = transform(customers)
-
-load(transformed_customers)
+# main guard - run this file when this file is executed directly
+def main():
+    customers = extract()
+    transformed_customers = transform(customers)
+    load(transformed_customers)
+    
+if __name__ == '__main__':
+    main()
